e57c968784a4b5b3e45a70edb9c9feddd3fe9f64
Dan F-M <danfm@nyu.edu>
2014-03-14 11:42:54 -0400

    object-oriented interface and compatibility

--- phist.py
+++ phist.py
@@ -1,13 +1,177 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This module provides an interface for finding the histogram of a set of noisy
+observations.
+
+"""
 
 from __future__ import division, print_function
 
-__all__ = ["phist"]
+__all__ = ["PHist", "phist"]
 
+__version__ = "0.0.1"
+
+__author__ = "Dan Foreman-Mackey (danfm@nyu.edu)"
+__contributors__ = []
+
+__copyright__ = "Copyright 2014 Daniel Foreman-Mackey"
+__license__ = "MIT"
+
+import logging
 import numpy as np
-import scipy.optimize as op
-from scipy.misc import logsumexp
+
+try:
+    import scipy.optimize as op
+except ImportError:
+    op = None
+
+try:
+    from scipy.misc import logsumexp
+except ImportError:
+    def logsumexp(a, axis=None, b=None):
+        """
+        This function is taken directly from scipy in order to support older
+        versions.
+
+        """
+        a = np.asarray(a)
+        if axis is None:
+            a = a.ravel()
+        else:
+            a = np.rollaxis(a, axis)
+        a_max = a.max(axis=0)
+        if b is not None:
+            b = np.asarray(b)
+            if axis is None:
+                b = b.ravel()
+            else:
+                b = np.rollaxis(b, axis)
+            out = np.log(sum(b * np.exp(a - a_max), axis=0))
+        else:
+            out = np.log(sum(np.exp(a - a_max), axis=0))
+        out += a_max
+        return out
+
+
+class PHist(object):
+    """
+    A probabilistic histogram.
+
+    :param x: ``(K, N)``
+        A list of ``N`` posterior samples for measurements of ``K`` examples.
+
+    :param bins:
+        Either the integer number of bins or an array of bin edges.
+
+    :param lnpriors: ``(K, N)`` or ``None``
+        If not ``None``, this should be an array with the log prior evaluated
+        at the samples given in ``x``.
+
+    """
+
+    def __init__(self, x, bins=10, lnpriors=None):
+        # Make sure that the samples have the correct shape.
+        x = np.atleast_2d(x)
+
+        # Interpret the bin specs.
+        try:
+            len(bins)
+        except TypeError:
+            bins = np.linspace(x.min(), x.max(), bins)
+        self.bins = np.array(bins)
+
+        # Pre-compute the bin indices of the samples.
+        self.inds = np.digitize(x.flatten(), bins).reshape(x.shape)
+
+        # Generate a prior array if one wasn't provided.
+        if lnpriors is None:
+            self.lnpriors = np.zeros_like(x)
+        else:
+            self.lnpriors = np.array(lnpriors)
+
+        # Make an initial guess at the histogram values.
+        self.theta = np.array(np.histogram(np.mean(x, axis=1), bins)[0],
+                              dtype=float)
+        m = self.theta > 0
+        self.theta[m] = np.log(self.theta[m])
+        self.theta[~m] = -np.inf
+
+        # Normalize the bin values to sum to one.
+        self.theta -= logsumexp(self.theta)
+
+    @property
+    def density(self):
+        """
+        The bin heights of the histogram normalized so that the integral over
+        all the bins is one.
+
+        """
+        norm = logsumexp(self.theta + np.log(self.bins[1:] - self.bins[:-1]))
+        return np.exp(self.theta - norm)
+
+    def lnlike(self, p):
+        """
+        Compute the marginalized log-likelihood of the samples given a set of
+        proposed bin heights.
+
+        :param p:
+            A list of bin heights. This must have length ``nbins-1``.
+
+        """
+        theta = np.empty(len(p)+2)
+        theta[1:-1] = p - logsumexp(p)
+        theta[0] = -np.inf
+        theta[-1] = -np.inf
+        return np.sum(logsumexp(theta[self.inds] - self.lnpriors, axis=1))
+
+    def nll(self, p):
+        """
+        Compute the negative marginalized log-likelihood of the data. This is
+        legitimately just the negative of the ``lnlike`` method.
+
+        """
+        return -self.lnlike(p)
+
+    def optimize(self, **kwargs):
+        """
+        Use ``scipy.optimize.minimize`` to find the maximum marginalized
+        likelihood histogram. In particular, this function uses the bounded
+        ``L-BFGS-B`` method.
+
+        :param ** kwargs:
+            Keyword arguments passed directly to the ``minimize`` call. Any
+            values for ``method`` or ``bounds`` will be ignored.
+
+        :returns result:
+            The results dictionary from the minimize call.
+
+        """
+        if op is None:
+            raise ImportError("Install scipy to optimize your phist.")
+
+        # Try and support older versions of scipy that don't have the
+        # "minimize" API.
+        if hasattr(op, "minimize"):
+            kwargs["method"] = "L-BFGS-B"
+            minimize = op.minimize
+        else:
+            logging.warn("Using legacy optimization interface. Consider "
+                         "upgrading scipy.")
+            kwargs.pop("method", None)
+            minimize = op.fmin_l_bfgs_b
+
+        # Ignore any provided bounds.
+        kwargs["bounds"] = [(None, 0)]*len(self.theta)
+
+        # Run the optimization to get the maximum likelihood histogram.
+        p0 = self.theta - logsumexp(self.theta)
+        results = minimize(self.nll, p0, **kwargs)
+
+        # Save the results of the optimization.
+        self.theta = np.array(results.x)
+
+        return results
 
 
 def phist(x, bins=10, lnpriors=None, verbose=False):
@@ -25,51 +189,9 @@ def phist(x, bins=10, lnpriors=None, verbose=False):
         at the samples given in ``x``.
 
     """
-    x = np.atleast_2d(x)
-
-    # Interpret the bin specs.
-    try:
-        len(bins)
-    except TypeError:
-        bins = np.linspace(x.min(), x.max(), bins)
-
-    # Pre-compute the bin indices of the samples.
-    inds = np.digitize(x.flatten(), bins).reshape(x.shape)
-
-    # Generate a prior array if one wasn't provided.
-    if lnpriors is None:
-        lnpriors = np.zeros_like(x)
-
-    # Make an initial guess at the histogram values.
-    theta = np.array(np.histogram(np.mean(x, axis=1), bins)[0], dtype=float)
-    theta /= np.sum(theta)
-    m = theta > 0
-    theta[m] = np.log(theta[m])
-    theta[~m] = -np.inf
-
-    # # FIXME: Start at a dumb place.
-    # theta = np.zeros_like(theta) - np.log(len(theta))
-
-    # Compute the maximum likelihood histogram.
-    results = op.minimize(_objective, theta, args=(inds, lnpriors),
-                          method="L-BFGS-B",
-                          bounds=[(None, 0)]*len(theta))
-    if verbose:
-        print(results)
-
-    # Normalize the distribution to a density.
-    density = np.exp(results.x)
-    density /= np.sum(density*(bins[1:] - bins[:-1]))
-
-    return density, bins
-
-
-def _objective(p, inds, lnpriors):
-    theta = np.empty(len(p)+2)
-    theta[1:-1] = p - logsumexp(p)
-    theta[0] = -np.inf
-    theta[-1] = -np.inf
-    return -np.sum(logsumexp(theta[inds] - lnpriors, axis=1))
+    p = PHist(x, bins=bins, lnpriors=lnpriors)
+    p.optimize()
+    return p.density, p.bins
 
 
 if __name__ == "__main__":
